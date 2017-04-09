d75143be7b22ab5402861f9fa022765dbbe92784
Rohan Prakash <rohanpj95@gmail.com>
2017-04-09 15:01:02 -0500

    My first commit

--- Swift Learning/ViewController.swift
+++ Swift Learning/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            theLabel.text = "You pushed 10 times."
+        }
+        
+    }
+    
+    @IBAction func button2Tap(_ sender: Any) {
+        theLabel.text = "Buttons are cool!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
